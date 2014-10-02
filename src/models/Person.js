16c9a3afbcc1f5a4ddf5127a21fc0b025f189a45
mlunoe <michael.lunoe@gmail.com>
2014-10-01 23:55:19 -0700

    Adding id

--- src/models/Person.js
+++ src/models/Person.js
@@ -1,6 +1,7 @@
 var Backbone = require("Backbone");
 
 module.exports = Backbone.Model.extend({
+  idAttribute: "id",
   initialize: function(data, options) {
     this.options = options;
     this.name = data.name;
