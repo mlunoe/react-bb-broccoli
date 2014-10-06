d4d35befff06b9cecf6bf8da4b37da9daca5fc9d
mlunoe <michael.lunoe@gmail.com>
2014-10-06 10:44:51 -0700

    Adding images in page to test md5

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 /** @jsx React.DOM */
 
 var React = require("react");
+
 var PeopleList = require("./components/PeopleList");
 
 /* jshint trailing:false, quotmark:false, newcap:false */
 React.renderComponent(
   <div>
-    <h1>Build with Broccoli</h1>
+    <h1>
+      Build with <img src="img/favicon.ico" />
+      Broccoli<img src="img/favicon.ico" />
+    </h1>
     <PeopleList />
   </div>,
   document.getElementById("application")
