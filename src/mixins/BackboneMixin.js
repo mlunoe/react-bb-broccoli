c600c824b6da13d060f9be2deb51732de5ac63d5
mlunoe <michael.lunoe@gmail.com>
2014-10-19 12:54:24 -0700

    Updating backbone mixing to autohandle common cases

--- src/mixins/BackboneMixin.js
+++ src/mixins/BackboneMixin.js
@@ -1,4 +1,21 @@
-module.exports = {
+var _ = require("underscore");
+
+var BackboneMixin = {
+  resolveModels: function() {
+    var models = [this.props.model, this.props.collection];
+
+    if (_.isFunction(this.getBackboneModels)) {
+      models = _.union(models, this.getBackboneModels());
+    }
+
+    models = _.filter(models, function(model) {
+      if (model != null) {
+        return model;
+      }
+    });
+
+    return models;
+  },
   // Binds a listener to a component's resource so the component can be updated
   // when the resource changes.
   //
@@ -7,18 +24,22 @@ module.exports = {
   // `Backbone.Model` and `Backbone.Collection`.
   componentDidMount: function() {
     this._boundForceUpdate = this.forceUpdate.bind(this, null);
-    this.getBackboneModels().forEach(function(model) {
+    var models = this.resolveModels();
+    models.forEach(function(model) {
       // There are more events that we can listen on. For most cases, we're
       // fetching pages of data, listening to add events causes superfluous
       // calls to render.
-      model.on("batch reset", this._boundForceUpdate, this);
+      model.on("batch reset sync", this._boundForceUpdate, this);
       model.fetch({ reset: true });
     }, this);
   },
 
   componentWillUnmount: function() {
-    this.getBackboneModels().forEach(function(model) {
-      model.off("batch reset", this._boundForceUpdate, this);
+    var models = this.resolveModels();
+    models.forEach(function(model) {
+      model.off("batch reset sync", this._boundForceUpdate, this);
     }, this);
   }
 };
+
+module.exports = BackboneMixin;
